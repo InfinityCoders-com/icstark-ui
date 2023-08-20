fe2a0a161ce3562a06b1e62d9a03ac7eac7c60fc
Infinity Coders <Infiniticoder@gmail.com>
2023-08-20 16:37:28 +0530

    Initialized nx react typescript storybook cypress

---
+++ src/app/app.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+const StyledApp = styled.div``
+
+export function App() {
+  return (
+    <StyledApp>
+      <h1>Welcome org</h1>
+    </StyledApp>
+  )
+}
+
+export default App
